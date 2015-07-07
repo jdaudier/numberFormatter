c12665fbd36a73b7133a8093d36366d046ead345
Joanne Daudier <jdaudier@outlook.com>
2015-07-07 19:17:56 -0400

    Add locale param

--- index.js
+++ index.js
@@ -3,8 +3,9 @@
 /**
  * Adds commas to a number
  * @param {number} number
+ * @param {string} locale
  * @return {string}
  */
-module.exports = function(number) {
-    return number.toLocaleString();
+module.exports = function(number, locale) {
+    return number.toLocaleString(locale);
 };
